e31a2371bcd05fa9bb98526a6b45f9a72b0a3833
Haleema Sadiq <haleemasadiq18@gmail.com>
2022-06-01 17:18:58 +0500

    login and users

--- frontend/src/app/api-services/login.service.ts
+++ frontend/src/app/api-services/login.service.ts
@@ -1,27 +1,16 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { baseUrl } from './../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private _http: HttpClient) { }
+
+  authenticateUser(data): Observable<any> {
+    return this._http.post(`${baseUrl}user/login`, data);
+  }
 }
-// import { Injectable } from '@angular/core';
-// import { HttpClient } from '@angular/common/http';
-// import { Observable } from 'rxjs';
-// import { baseUrl } from './../../environments/environment';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class LoginService {
-
-//   constructor(private _http: HttpClient) { }
-
-//   authenticateUser(data): Observable<any> {
-//     return this._http.post(`${baseUrl}user/login`, data);
-//   }
-
-
-// }
