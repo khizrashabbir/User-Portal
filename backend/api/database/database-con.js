1810d807b6a71eb3f56c0bb215659a99e1d34c23
AnonymousISI <labzaik@gmail.com>
2022-06-01 09:02:49 +0500

    backend query

--- backend/api/database/database-con.js
+++ backend/api/database/database-con.js
@@ -47,3 +47,40 @@ module.exports.queryData = function (getQuery,valuesArr) {
     });
   });
 }
+/// add data in users
+module.exports.addData = function (insertQuery, valuesArr, res, log_obj) {
+  let query = mysql.format(insertQuery, valuesArr);
+
+  pool.query(query, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.status(400).json("Error! cannot connect to database");
+      return;
+    }
+    // addLog(log_obj);
+    res.status(200).json("Record Added Successfully!");
+  });
+}
+
+module.exports.addLog = function (obj) {
+  addLog(obj);
+}
+///Update users data
+
+module.exports.updateData = function (updateQuery, valuesArr, res, log_obj) {
+  let query = mysql.format(updateQuery, valuesArr);
+  console.log(query);
+  pool.query(query, (err, data) => {
+    console.log(data);
+    if (err) {
+      console.log(err);
+      res.status(400).json("Error! cannot connect to database");
+      return;
+    } else if (data.affectedRows > 0) {
+      addLog(log_obj);
+      res.status(200).json("Record Updated Successfully!");
+    } else {
+      res.status(404).json("Record Not Found");
+    }
+  });
+}
